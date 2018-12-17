a7972ba7503b7dc3e0887a3b20ab0628f6d57e48
Steven DeVere <sdevere@xogrp.com>
2018-12-16 22:17:27 -0500

    feat: applyMiddleware function

--- src/createStore.js
+++ src/createStore.js
@@ -1,10 +1,14 @@
 const Store = require('./Store')
 const State = require('./State')
 
-const createStore = (reducer) => {
+const createStore = (reducer, middleware) => {
   const store = new Store(reducer)
 
   store.getAccessToState(new State())
+  if (middleware) {
+    let { dispatch, getState } = store
+    dispatch = middleware(dispatch.bind(store), getState)
+  }
   return store
 }
 
--- tests/sandbox.js
+++ tests/sandbox.js
@@ -1,13 +1,18 @@
 const createStore = require('../src/createStore')
 const testReducers = require('./utils/testReducer')
 const combineReducers = require('../src/combineReducers')
-
+const applyMiddleware = require('../src/applyMiddleware')
 
 const { catReducer, dogReducer } = testReducers
 
 const reducer = combineReducers({ cat: catReducer, dog: dogReducer })
 
-const store = createStore(reducer)
+const testMiddleware = (action, dispatch, getState) => {
+  console.log('hello i am middleware')
+  dispatch(action)
+}
+
+const store = createStore(reducer, applyMiddleware(testMiddleware))
 
 console.log('first state', store.getState())
 store.dispatch({ type: 'sad' })
